d8c7fd17b0e64d2370f2972c46d3fa172bd7bc63
mbrazeau <mbrazeau@lb-mbrazeau.sp.ic.ac.uk>
2017-05-09 08:13:38 +0100

    ++comments; made whitespacing nice for documentation/reading API

--- mpl.h
+++ mpl.h
@@ -36,10 +36,10 @@
 #define mpl_h
 
 
-#ifdef __CPLUSPLUS
+#ifdef __cplusplus
 extern "C" {
 #endif /*__CPLUSPLUS */
-
+#include <stdbool.h>
 #include "morphydefs.h"
 #include "mplerror.h"
     
@@ -54,6 +54,7 @@ extern "C" {
  @return A void pointer to the Morphy instance. NULL if unsuccessful.
  */
 Morphy  mpl_new_Morphy
+    
         (void);
 
 
@@ -62,11 +63,13 @@ Morphy  mpl_new_Morphy
 
  @discussion Destroys an instance of the Morphy object, calling all destructors
  for internal object completely returning the memory to the system.
- @param m An Morphy object to be destroyed.
+ @param m A Morphy object to be destroyed.
  @return A Morphy error code.
  */
-int mpl_delete_Morphy
-    (Morphy m);
+int     mpl_delete_Morphy
+    
+        (Morphy m);
+
 
 /*!
 @brief Sets up the dimensions of the dataset.
@@ -75,48 +78,201 @@ int mpl_delete_Morphy
 @param ntax The number of taxa (or tips/terminals).
 @param nchar The number of characters (i.e. transformation series) in the 
  data set.
+ @param m An instance of the Morphy object.
 @return Morphy error code.
 */
-int mpl_init_Morphy
-    (const int  ntax,
-     const int  nchar,
-     Morphy     m);
+int     mpl_init_Morphy
     
+        (const int  ntax,
+         const int  nchar,
+         Morphy     m);
     
-int     mpl_get_numtaxa         (Morphy m);
-int     mpl_get_num_charac      (Morphy m);
-int     mpl_attach_symbols      (const char* symbols, Morphy m);
-char*   mpl_get_symbols         (const Morphy m);
-int     mpl_attach_rawdata      (const char* rawmatrix, Morphy m);
-int     mpl_delete_rawdata      (Morphy m);
-int     mpl_set_gap_symbol      (const char gapsymb, Morphy m);
-int     mpl_set_missing_symbol  (const char missymb, Morphy m);
 
-int     mpl_set_postorder       (const int nodeID, const int index, Morphy m);
+/*!
+@brief Retrieve the number of taxa (rows) in the dataset.
+@discussion Retrieves the number of taxa (rows) in the dataset.
+@param m An instance of the Morphy object.
+@return The number of taxa if success, otherwise an error code.
+*/
+int     mpl_get_numtaxa
+    
+        (Morphy m);
 
-int     mpl_incl_charac         (const int charID, Morphy m);
-int     mpl_excl_charac         (const int charID, Morphy m);
 
-int     mpl_set_charac_weight   (const int charID, Mflt weight);
+/*!
+@brief Retrieve the number of taxa (rows) in the dataset.
+@discussion Retrieves the number of taxa (rows) in the dataset.
+@param m An instance of the Morphy object.
+@return The number of taxa if success, otherwise an error code.
+*/
+int     mpl_get_num_charac
+    
+        (Morphy     m);
+    
 
-int     mpl_set_parsim_t        (const int charID, const MPLptype_t parsfxn, Morphy m);
-int     mpl_set_gaphandl        (const gap_t gaptype, Morphy m);
-int     mpl_query_gaphandl      (Morphy m);
+/*!
+@brief Attach a caller-specified list of symbols.
+@discussion Allows the caller to specify a list of symbols in the data matrix,
+ otherwise, the symbols list used by Morphy will be extracted from the matrix. 
+ The symbols list must match the symbols provided in the matrix. When Morphy 
+ extracts symbols from the matrix, their ordering is alphanumeric, according to
+ their ASCII codes (i.e. "+0123...ABCD...abcd..."). Loading a user-specified
+ symbols list will override this ordering. Symbols loaded in either the list or
+ the matrix must be valid Morphy character state symbols as defined in the 
+ statedata.h header file.
+@param symbols A C-style (i.e. NULL-terminated) string of valid state symbols.
+@param m An instance of the Morphy object.
+@return Morphy error code.
+*/
+int     mpl_attach_symbols
+    
+        (const char*    symbols,
+         Morphy         m);
+    
 
-int     mpl_down_recon          (const int nodeID, const int lChild, const int rChild, Morphy m);
-int     mpl_up_recon            (const int nodeID, const int lChild, const int rChild, const int parentID, Morphy m);
-int     mpl_up_final_recon      (const int nodeID, const int lChild, const int rChild, const int parentID, Morphy m);
-int     mpl_update_tip          (const int nodeID, Morphy m);
-int     mpl_finalize_tip        (const int nodeID, Morphy m);
+/*!
+ @brief Retrieves the current list of symbols.
+ @discussion Returns a pointer to the string of character state symbols 
+ currently being used by Morphy (i.e. either the list of symbols extracted
+ from the matrix, or the caller-specified values).
+ @param m An instance of the Morphy object.
+ @return A C-style (null-terminated) string of the character state symbols being
+ used. NULL if failure.
+ */
+char*   mpl_get_symbols
 
-int     mpl_get_insertcost_max  (const int srcID, const int tgt1ID, const int tgt2ID, Morphy m);
-int     mpl_get_insertcost_min  (const int srcID, const int tgt1ID, const int tgt2ID, Morphy m);
+        (const Morphy   m);
 
-int     mpl_get_packed_states   (const int nodeID, const int character, int passnum, Morphy m);
-int     mpl_get_stateset        (char* res, const int nodeID, const int character, int passnum, Morphy m);
 
-#ifdef __CPLUSPLUS
+int     mpl_attach_rawdata
+
+        (const char*    rawmatrix,
+         Morphy         m);
+
+
+int     mpl_delete_rawdata
+
+        (Morphy     m);
+
+
+int     mpl_set_gap_symbol
+
+        (const char gapsymb,
+         Morphy     m);
+
+
+int     mpl_set_missing_symbol
+        (const char missymb,
+         Morphy     m);
+
+
+int     mpl_set_postorder
+
+        (const int  nodeID,
+         const int  index,
+         Morphy     m);
+
+
+int     mpl_incl_charac
+
+        (const int  charID,
+         Morphy     m);
+
+
+int     mpl_excl_charac
+
+        (const int  charID,
+         Morphy     m);
+
+
+int     mpl_set_charac_weight
+
+        (const int  charID,
+         Mflt       weight);
+
+
+int     mpl_set_parsim_t
+
+        (const int charID,
+         const MPLptype_t parsfxn,
+         Morphy           m);
+
+
+int     mpl_set_gaphandl
+
+        (const gap_t gaptype,
+         Morphy      m);
+
+
+int     mpl_query_gaphandl
+
+        (Morphy     m);
+
+
+int     mpl_down_recon
+
+        (const int  nodeID,
+         const int  lChild,
+         const int  rChild,
+         Morphy     m);
+
+
+int     mpl_up_recon
+
+        (const int  nodeID,
+         const int  lChild,
+         const int  rChild,
+         const int  parentID,
+         Morphy     m);
+
+
+int     mpl_up_final_recon
+
+        (const int  nodeID,
+         const int  lChild,
+         const int  rChild,
+         const int  parentID,
+         Morphy     m);
+
+
+int     mpl_update_tip
+
+        (const int  nodeID,
+         Morphy     m);
+
+
+int     mpl_finalize_tip
+
+        (const int  nodeID,
+         Morphy     m);
+
+int     mpl_get_insertcost
+
+        (const int  srcID,
+         const int  tgt1ID,
+         const int  tgt2ID,
+         bool       max,
+         Morphy     m);
+
+
+int     mpl_get_packed_states
+
+        (const int  nodeID,
+         const int  character,
+         int        passnum,
+         Morphy     m);
+
+
+int     mpl_get_stateset
+
+        (char*      res,
+         const int  nodeID,
+         const int  character,
+         int        passnum,
+         Morphy     m);
+
+#ifdef __cplusplus
 }
-#endif /*__CPLUSPLUS */
+#endif /*__cplusplus */
 
 #endif /* mpl_h */
