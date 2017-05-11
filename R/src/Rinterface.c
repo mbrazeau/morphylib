2e6580c3b96fd56ee6367dc186c8351830ce0023
Thomas Guillerme <guillert@tcd.ie>
2017-05-11 14:55:37 +0100

    R interface INIT

---
+++ R/src/Rinterface.c
@@ -0,0 +1,18 @@
+
+
+char(*) matrix
+int ntaxa
+int nchar
+int(*) list_anc
+int(*) list_child
+int(*) weight
+
+
+SEXP morphy_handler(SEXP matrix, SEXP ntaxa, SEXP nchar, SEXP list_anc, SEXP list_child, SEXP weight) {
+
+    int nedges
+    int nnodes
+    char(*) symbols
+    
+
+}
