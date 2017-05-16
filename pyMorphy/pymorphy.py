f479bede2170af81dc93811fed347ba4e43cad77
mbrazeau <mbrazeau@lb-mbrazeau.sp.ic.ac.uk>
2017-05-16 13:22:30 +0100

    changed namespace identifier

--- pyMorphy/pymorphy.py
+++ pyMorphy/pymorphy.py
@@ -1,15 +1,15 @@
 from ctypes import *
 cdll.LoadLibrary("../libmorphy_x86_64-Darwin.so")
-Morphy = CDLL("../libmorphy_x86_64-Darwin.so");
+morphylib = CDLL("../libmorphy_x86_64-Darwin.so");
 
-Morphy.mpl_new_Morphy.argtypes = []
-Morphy.mpl_new_Morphy.restype = c_void_p
+morphylib.mpl_new_Morphy.argtypes = []
+morphylib.mpl_new_Morphy.restype = c_void_p
 
-Morphy.mpl_delete_Morphy.argtypes = [c_void_p]
-Morphy.mpl_delete_Morphy.restype = c_int
+morphylib.mpl_delete_Morphy.argtypes = [c_void_p]
+morphylib.mpl_delete_Morphy.restype = c_int
 
-Morphy.mpl_init_Morphy.argtypes = [c_int, c_int, c_void_p]
-Morphy.mpl_init_Morphy.restype = c_int
+morphylib.mpl_init_Morphy.argtypes = [c_int, c_int, c_void_p]
+morphylib.mpl_init_Morphy.restype = c_int
 
 
 
