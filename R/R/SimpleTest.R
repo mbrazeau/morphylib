b52d70bf0d4e6b09cbcc542c237fafae9baf8283
Martin R. Smith <martins@gmail.com>
2017-05-19 17:12:16 +0100

    Tree scores now calculated using RMorphyEx.C AND (efficiently) RMorphy.C

--- R/R/SimpleTest.R
+++ R/R/SimpleTest.R
@@ -1,12 +1,17 @@
 library('phangorn')
 tree <- read.tree(text="(((a, b), c), (d, (e, f)));")
-plot(tree); nodelabels(); tiplabels();
+plot(tree); nodelabels(6:10); tiplabels(0:5);
 
 tax.names <- letters[1:6]
 n.tip <- length(tax.names)
 n.char <- 4
 weight <- c(1,2,3,4)
-matrix_as_string <- '001100 00---1 0+?-10 01---0;'
+matrix_as_grid <- t(matrix(c('0', '0', '1', '1', '0', '0',
+                             '0', '0', '-', '-', '-', '1',
+                             '0', '+', '?', '-', '1', '0', 
+                             '0', '1', '-', '-', '-', '0'), nrow=4, ncol=6, byrow=T))
+                    
+matrix_as_string <- paste(c(matrix_as_grid, ';'), collapse='')
 edge <- tree$edge
 parent <- edge[, 1]
 child  <- edge[, 2]
@@ -26,12 +31,26 @@ preorder <- root.node:max.node
 ancestor <- function (x) parent[child==x]
 descendant <- function (x) child[parent==x]
 ancestors <- as.integer(c(vapply(1:n.tip, ancestor, double(1)), 0, vapply((n.tip + 2):(n.tip * 2 - 1), ancestor, double(1))))
+ancestors[root.node] <- dummy.root.node
 descendants <- as.integer(vapply(preorder, descendant, double(2))) # children of each node, a pair at a time, right-left, right-left
 right_ids <- left_ids <- integer(max.node)
 odds <- as.logical(seq_along(descendants) %% 2)
 right_ids[root.node:max.node] <- descendants[odds]
  left_ids[root.node:max.node] <- descendants[!odds]
 
+ 
+dyn.load('../src/RMorphy.dll')
+CallMorphy <- function (n_char, n_taxa, desc, ancs, rawmatrix) {
+  .Call('RMorphy', as.integer(n_char), as.integer(n_taxa), as.integer(desc - 1L),
+                  as.integer(ancs - 1L), as.character(rawmatrix))
+}
+CallMorphy(n.char, n.tip, descendants, ancestors, matrix_as_string)
+dyn.unload('../src/RMorphy.dll')  
+ 
+ 
+ 
+ 
+ 
 dyn.load("../src/RMorphyEx.dll")
 source("../src/Morphyex.R")
 morphy <- mpl_new_Morphy()
@@ -46,26 +65,27 @@ if (mpl_set_num_internal_nodes(n.internal, morphy)) stop ("mpl_set_num_internal_
 if (mpl_get_num_internal_nodes(morphy) != n.internal) stop('mpl_get_num_internal_nodes mismatch')
 if (err <- mpl_apply_tipdata(morphy)) stop("mpl_apply_tipdata failed", err)
 
+score <- 0
 for (node in postorder) {
-  score <- mpl_first_down_recon(node - 1L, left_ids[node] - 1L, right_ids[node] - 1L, morphy)
+  score <- score + mpl_first_down_recon(node - 1L, left_ids[node] - 1L, right_ids[node] - 1L, morphy)
   #cat("Node ", node, "left: ", left_ids[node], " right, ", right_ids[node], "\n")
 }
 score
 if (mpl_update_lower_root(dummy.root.node - 1L, root.node - 1L, morphy)) stop ("mpl_update_lower_root Error")
 for (node in preorder[-1]) {
   #cat("Node:", node, "left:", left_ids[node], " right:", right_ids[node], "anc:", ancestors[node], "\n")
-  score <- mpl_first_up_recon(node - 1L,  left_ids[node] - 1L, right_ids[node] - 1L, ancestors[node] - 1L, morphy) 
+  score <- score + mpl_first_up_recon(node - 1L,  left_ids[node] - 1L, right_ids[node] - 1L, ancestors[node] - 1L, morphy) 
 }
 score
 for (tip in tips) {
   mpl_update_tip(tip - 1L, ancestors[tip] - 1L, morphy)
 }
 for (node in postorder) {
-  score <- mpl_second_down_recon(node - 1L, left_ids[node] - 1L, right_ids[node] - 1L, morphy)
+  score <- score + mpl_second_down_recon(node - 1L, left_ids[node] - 1L, right_ids[node] - 1L, morphy)
 }
 if (mpl_update_lower_root(dummy.root.node - 1L, root.node - 1L, morphy)) stop ("Second mpl_update_lower_root Error")
 for (node in preorder[-1]) {
-  score <- mpl_second_up_recon(node - 1L,  left_ids[node] - 1L, right_ids[node] - 1L, ancestors[node] - 1L, morphy) 
+  score <- score +mpl_second_up_recon(node - 1L,  left_ids[node] - 1L, right_ids[node] - 1L, ancestors[node] - 1L, morphy) 
 }
 score
 
--- R/src/RMorphy.c
+++ R/src/RMorphy.c
@@ -9,6 +9,8 @@
 #include <R.h>
 #include <Rinternals.h>
 #include <Rmath.h>
+#include "../../src/mpl.h"
+#include "RMorphyUtils.h"
 
 
 // Morphy handler for R
@@ -18,108 +20,98 @@
 // This will probably be quick enough to allow tree search to proceed in R, even without 
 // subsequent optimisations (e.g. using 'normal' fitch optimization for characters with <2 inapp tokens)
 
-SEXP Morphy(SEXP r_n_char, SEXP r_n_taxa, SEXP r_descendants, SEXP r_ancestors, SEXP r_rawmatrix) {
+SEXP RMorphy(SEXP r_n_char, SEXP r_n_taxa, SEXP r_descendants, SEXP r_ancestors, SEXP r_rawmatrix) {
   
   // Convert arguments passed from R into C variables
   
   int n_char=asInteger(r_n_char), n_taxa=asInteger(r_n_taxa);  // asInteger converts length 1 R vector into scalar
+  // r_descendants and r_ancestors have already had one subtracted to convert them to an index 
   int *descendants=INTEGER(r_descendants), *ancestors=INTEGER(r_ancestors);  // INTEGER gives pointer to first element of length n R vector
   const char *rawmatrix=CHAR(asChar(r_rawmatrix));
  
-  Rprintf("Hello, world! I read some data!\n");
-  Rprintf(" - nchar = %i, ntaxa = %i \n", n_char, n_taxa);
   Rprintf(" - character string = %s\n", rawmatrix);
   
   // Calculate relevant properties of the tree and dataset
-  int n_internal = n_taxa - 1;
-  int max_node = n_taxa + n_internal;
-  Rprintf(" - %i nodes, of which %i are internal, %i are tips, and one is the root\n", max_node, n_internal, n_taxa);
+  int n_internal = n_taxa; // One more than you might expect because there's a dummy root node
+  int root_node = n_taxa;
+  int max_node = n_taxa + n_internal - 1L;
+  Rprintf(" - %i nodes + dummy root, of which %i are internal (plus dummy root), %i are tips\n", max_node, n_internal - 1L, n_taxa);
   
   // Declare and protect result, to return to R
-  SEXP RESULT, node_parent, node_children;
+  SEXP RESULT, pscore, node_children;
   PROTECT(RESULT = allocVector(VECSXP, 2));
-  PROTECT(node_parent = allocVector(INTSXP, 1));
+  PROTECT(pscore = allocVector(INTSXP, 1));
   PROTECT(node_children = allocVector(INTSXP, 2));
   
   // Initialize return variables
   int i;
-  int *parent_temp, *children_temp;
-  //INTEGER(node_parent)[0] = 0;
-  parent_temp = INTEGER(node_parent);
+  int *pscore_temp, *children_temp;
+  pscore_temp = INTEGER(pscore);
+  *pscore_temp = 0;
   children_temp = INTEGER(node_children);
   for (i = 0; i < 2; i++) {
     children_temp[i] = 0;
   }
     
-  
   // This is just to test that the ancestor identification is working as expected
   int root_node_left_child = n_taxa + 1;
-  *parent_temp = ancestors[root_node_left_child];
   children_temp[0] = descendants[(root_node_left_child - n_taxa) * 2];
   children_temp[1] = descendants[((root_node_left_child - n_taxa) * 2)+ 1];
   
-  Rprintf(" - Root node's left child has node %i as its ancestor, and %i and %i as its left and right children\n",
-    parent_temp, children_temp[0], children_temp[1]
+  Rprintf(" - Root node's left child has nodes %i and %i as its left and right children\n",
+    children_temp[0], children_temp[1]
   );
   
-  SET_VECTOR_ELT(RESULT, 0, node_parent);
+  Morphy handl = mpl_new_Morphy();
+  mpl_init_Morphy(n_taxa, n_char, handl);
+ 
+  for (i = 0; i < n_char; i++) {
+    mpl_set_parsim_t(i, FITCH_T, handl);
+  }
+  mpl_set_num_internal_nodes(n_internal, handl);
+  mpl_attach_rawdata(rawmatrix, handl);
+  mpl_apply_tipdata(handl);
+  Rprintf(" - Initialized morphy object with %i characters and %i taxa.\n - Begin traversal:\n", mpl_get_num_charac(handl), mpl_get_numtaxa(handl));
+  
+ 
+  for (i = max_node - 1L; i > n_taxa; i--) { // First Downpass 
+  // First node is called 11 in R, it's numbered 10 in C
+  // Max node is 11, so we knock one off to give the node's index, i = 10
+  // Stop before i == n_taxa (which would be the root node)
+  
+  // i == 11
+  // If i == 6 (root node), descendants will be in positions [0, 1].  i - n_taxa = 0
+  // If i == 11, descendants will be in positions [8, 9]. i - n_taxa = 4
+  
+    Rprintf("   - Reconstructiing  node %i < %i,%i.\n", i, descendants[(i - n_taxa) * 2], descendants[((i - n_taxa) * 2) + 1]);
+    *pscore_temp += mpl_first_down_recon(i,  descendants[(i - n_taxa) * 2], descendants[((i - n_taxa) * 2) + 1], handl);
+  }
+  mpl_update_lower_root(max_node, root_node, handl);
+ 
+  for (i = root_node; i < max_node; i++) { // First uppass: internal nodes
+    *pscore_temp += mpl_first_up_recon(i, descendants[(i - n_taxa) * 2], descendants[((i - n_taxa) * 2) + 1], ancestors[i], handl);
+  }
+  for (i = 0; i < n_taxa; i++) { // First uppass: update tips
+    mpl_update_tip(i, ancestors[i], handl);
+  }
+  
+  for (i = max_node - 1L; i > n_taxa; i--) { // Second Downpass 
+    *pscore_temp += mpl_second_down_recon(i, descendants[(i - n_taxa) * 2], descendants[((i - n_taxa) * 2) + 1], handl);
+  }
+ 
+  for (i = n_taxa; i < max_node; i++) { // Second uppass: internal nodes
+    *pscore_temp += mpl_second_up_recon(i, descendants[(i - n_taxa) * 2], descendants[((i - n_taxa) * 2) + 1], ancestors[i], handl);
+  }
+  for (i = 0; i < n_taxa; i++) { // Second uppass: finalize tips (fwiw)
+    mpl_finalize_tip(i, ancestors[i], handl);
+  }
+  
+  Rprintf("\n Traversal complete.\n - Deleting Morphy handle\n");
+  mpl_delete_Morphy(handl);
+  SET_VECTOR_ELT(RESULT, 0, pscore);
   SET_VECTOR_ELT(RESULT, 1, node_children);
   UNPROTECT(3);
   return (RESULT);
   
-  
-  // Run relevant functions from Morphy Library
-  
-  // Some of said functions are as yet unwritten, others I'm not quite clear how to call (or in what order)
-  // I'm sure that MDB will be able to flesh this out very quickly.
-  /*
-  morphy = mpl_new_Morphy(); 
-  mpl_init_Morphy(&n_taxa, &n_char, morphy);
-  mpl_attach_rawmatrix(rawmatrix, morphy);
-  
-  for (i = 0; i < *n_char; i++) {
-    mpl_set_parsim_t(i, FITCH_T, morphy);
-  }
-  mpl_apply_tipdata(morphinstance);
- 
-  for (i = max_node - 1; i > n_taxa; i--) { // First Downpass 
-    mpl_NA_fitch_first_downpass(&i, descendants + (i - n_taxa * 2), descendants + ((i * 2) + 1), ancestors + i)
-  }
- 
-  for (i = n_taxa; i < max_node; i++) { // First uppass: internal nodes
-    mpl_NA_fitch_first_uppass(&i, descendants + (i * 2), descendants((i * 2) + 1), ancestors + i)
-  }
- 
-  for (i = 0; i < n_taxa; i++) { // First uppass: update tips
-    mpl_update_tip(&i, ancestors + i)
-  }
- 
-  mpl_prelim_recon(morphy);
-  
-  for (i = max_node - 1; i > n_taxa; i--) { // Second Downpass 
-    mpl_NA_fitch_second_downpass(&i, descendants + (i * 2), descendants + ((i * 2) + 1), ancestors + i)
-  }
- 
-  for (i = n_taxa; i < max_node; i++) { // Second uppass: internal nodes
-    mpl_NA_fitch_first_uppass(&i, descendants + (i * 2), descendants((i * 2) + 1), ancestors + i)
-  }
-  
- for (i = 0; i < n_taxa; i++) { // Second uppass: update tips.  (This is probably redundant.)
-    mpl_finalize_tip(&i, ancestors + i) 
-  }
-  
-  mpl_final_recon();
-  mpl_store_final_active_states();
-  
-  
-  // store variables in an format that can be returned to R
-  SET_VECTOR_ELT(RESULT, 0, pscore);
- 
-  // Unprotect allocated memory
- UNPROTECT(0); // TODO update to correct value
- 
-  // And return.
-  return(RESULT);
-  */
   return R_NilValue;
 }
