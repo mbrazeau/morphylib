7c55b7d8f1daed9211fd7b2ed4b492d41733141d
Martin R. Smith <martins@gmail.com>
2017-05-19 15:26:16 +0100

    A simple test case
    Score = 0!

--- R/R/SimpleTest.R
+++ R/R/SimpleTest.R
@@ -1,63 +1,12 @@
-#' @title R to morphy interface
-#'
-#' @description Uses the MorPhy library to calcuate the length of a phylogenetic tree
-#'
-#' @param \list {
-#'   \item{tree} A tree in \code{PhyDat} format;
-#'   \item{data} A object of class phyDat containing sequences
-#'   \item{site} return either 'pscore' or 'site' wise parsimony scores
-#' 
-#' @examples
-#' ## Load a tree and a matrix
-#' tree <- ape::read.tree(text = "((a,b),(c,d));")
-#' matrix <- matrix(c('0', '1', '-', '-', '?', '{01}', '0', '1'), nrow=2, ncol=2)
-#' 
-#' ## Calculate the length
-#' morphy.length(tree, matrix, site='pscore')
-#' 
-#' @seealso \code{\link{apply.reconstruction}}
-#' 
-#' @author Thomas Guillerme, Martin Smith
-#' @export
-
 library('phangorn')
-  dat <- read.nexus.data('C:/Work/parsimony_inapp/capaetal/capa.nex')  ## Use phyDat
- data <- phyDat(dat, type='USER', levels=unlist(strsplit('+012345678q-', '')))
- tax.names <- names(data)
- n.tip <- length(tax.names)
-tree <- rtree(n.tip, tip.label=tax.names, br=NULL)
+tree <- read.tree(text="(((a, b), c), (d, (e, f)));")
+plot(tree); nodelabels(); tiplabels();
 
-
-if (class(data) == 'phyDat') {
-  tax.names <- names(data)
-  n.char <- length(data[[1]])
-  weight <- attr(data, 'weight')
-  contrast <- attr(data, 'contrast')
-  dim.contrast <- dim(contrast)
-  nrow.contrast <- dim.contrast[1]
-  contrast <- matrix(as.logical(contrast), nrow.contrast, dim.contrast[2])
-  levels <- attr(data, 'levels')
-  dat <- vapply(data, as.integer, data[[1]])
-  matrix_as_string <- paste0(c(vapply(as.integer(unlist(data)), function (x) {
-    if (x == nrow.contrast) return('?')
-    tokens <- levels[contrast[x ,]]
-    if (length(tokens) > 1) return (paste0('{', tokens, '}', collapse=''))
-    return (tokens)
-  }, character(1)), ';'), collapse='')
-} else if (class(data) == 'list') {
-  tax.names <- rownames(dat)
-  dim.dat <- dim(dat.matrix)
-  n.char <- dim.dat[1]
-  n.tip  <- dim.dat[2]
-  dat.matrix <- vapply(dat, as.vector, dat[[1]])
-  matrix_as_string <- paste0(t(dat.matrix), collapse='')
-  weight <- rep(1, n.char)
-} else {
-  stop ("Unrecognized data format")
-}
-
-
+tax.names <- letters[1:6]
 n.tip <- length(tax.names)
+n.char <- 4
+weight <- c(1,2,3,4)
+matrix_as_string <- '001100 00---1 0+?-10 01---0;'
 edge <- tree$edge
 parent <- edge[, 1]
 child  <- edge[, 2]
@@ -91,7 +40,7 @@ if (mpl_get_numtaxa(morphy) != n.tip) warning("Taxon count mismatch with mpl_get
 if (mpl_get_num_charac(morphy) != n.char) warning("Character count mismatch in mpl_get_num_charac")
 if (msg <- mpl_attach_rawdata(as.character(matrix_as_string), morphy)) stop("mpl_attach_rawdata Couldn't attach symbols: error", msg)
 for (i in (1:n.char) - 1) if (msg <- mpl_set_parsim_t(i, tname=as.character("FITCH"), morphy)) stop("mpl_set_parsim_t error with char", i, msg)
-if (mpl_get_symbols(morphy) != paste0(levels[levels != '-'], collapse='')) stop ("Symbols mismatch in mpl_get_symbols")
+if (mpl_get_symbols(morphy) != '+01') stop ("Symbols mismatch in mpl_get_symbols")
 
 if (mpl_set_num_internal_nodes(n.internal, morphy)) stop ("mpl_set_num_internal_nodes failed")
 if (mpl_get_num_internal_nodes(morphy) != n.internal) stop('mpl_get_num_internal_nodes mismatch')
@@ -121,9 +70,3 @@ for (node in preorder[-1]) {
 score
 
 mpl_delete_Morphy(morphy)
-
-#ret <- .Call('Morphy', as.integer(n.char), as.integer(n.tip), as.integer(descendants), as.integer(ancestors), as.character(matrix_as_string))
-#ret # Returns a list with two entries: [1], the root node; [2], the descendants of the root node's left child.
-
-
-
