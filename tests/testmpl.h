2be458b7278ef6fc82a2c6138962050482bc8ec8
mbrazeau <mbrazeau@mbrazeaus-mbp.home>
2017-05-21 15:06:52 +0100

    fixed typo in prototype

--- tests/testmpl.h
+++ tests/testmpl.h
@@ -15,7 +15,7 @@ int test_reinit_no_data(void);
 int test_reinit_with_data(void);
 int test_simple_chtype_setting(void);
 int test_delete_Morphy_no_input(void);
-int test_deletiono_success(void);
+int test_deletion_success(void);
 int test_attemp_load_bad_dimens(void);
 
 int test_state_retrieval(void);
