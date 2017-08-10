c8469dbad98a5786dfc9aa5b49bab65fd47f5d95
mbrazeau <martin.brazeau@gmail.com>
2017-08-10 16:35:50 +0100

    Renaming; +notes on how to develop fxn

--- tests/testtreeopt.c
+++ tests/testtreeopt.c
@@ -68,7 +68,7 @@ int test_do_fullpass_on_tree(TLtree* t, Morphy m)
     return length;
 }
 
-int test_partial_downpass_for_inapplicables(TLtree* t, Morphy m)
+int test_full_reoptimization_for_inapplics(TLtree* t, Morphy m)
 {
     int end = 0;
     int length = 0;
@@ -78,11 +78,11 @@ int test_partial_downpass_for_inapplicables(TLtree* t, Morphy m)
     tl_traverse_tree(t->start, &index, postorder);
     end = index-1;
     printf("\n");
-    
+    TLnode* n = NULL;
     int i = 0;
     for (i = 0; i <= end; ++i) {
         
-        TLnode* n = &t->trnodes[postorder[i]];
+        n = &t->trnodes[postorder[i]];
         
         if (!n->tip) {
             // Do the eoptimzation
@@ -92,18 +92,65 @@ int test_partial_downpass_for_inapplicables(TLtree* t, Morphy m)
             // If not, break the loop
             // Check if at the root of the tree; if so, update the root
         }
-        
-        if (i == end) {
-            // Do the root.
-        }
+    }
+    
+    if (i == end) {
+        // Do the root;
     }
     
     // Now proceed all the way upward until at least 1 node past the clip insertion
     // (For optimisation, can push these nodes to a new list)
     // Then from the new list, pass all the way down, adding steps
     // And finally all the way back up again, adding steps.
+    // The extent of this uppass can probably be determined by the downpass
+    // path, partially. When the uppass finishes along a branch it can 'snap'
+    // back to the downpass path and continue from there.
     
     // Once it's all over, restore the state sets to their original values.
     // Reset the partitions' number of NA chars to update
+    
+    for (i = end; i >= 0; ++i) {
+        
+        n = &t->trnodes[postorder[i]];
+        
+        if (n->tip > 0) {
+            // Do a tip [need update function for this];
+        }
+        else {
+            // Do first uppass reoptimization
+        }
+    }
+    
+    // Update the tips:
+    // TODO: call tip updater
+    
+    
+    // Do the second downpass;
+    for (i = 0; i <= end; ++i) {
+        // Apply downpass calculations
+        // Collect steps added
+        // Colelct steps taken back
+    }
+   
+    // This function can limit its depth to the last node updated by the second
+    // downpass function; I don't think it can limit its depth to the last
+    // node updated by the first downpass update.
+    
+    for (i = end; i >= 0; ++i) {
+        // Do the second uppass;
+        n = &t->trnodes[postorder[i]];
+        
+        // Apply uppass calculations
+        if (n->tip > 0) {
+            // Do a tip [need update function for this];
+            // Might be possible to skip it.
+        }
+        else {
+            // Do first uppass reoptimization
+            // Collect steps added
+            // Collect steps taken back.
+        }
+    }
+    
     return length;
 }
